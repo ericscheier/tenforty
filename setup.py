78db2b64b5764763506d7e22afedb66652dbac87
Mike Macpherson <mmacpherson@users.noreply.github.com>
2024-02-16 16:01:34 -0800

    try declaring pacakges in setuptools

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 """Setup for tenforty package."""
 
 from Cython.Build import cythonize
-from setuptools import Extension, find_packages, setup
+from setuptools import Extension, setup
 
 setup(
-    ext_modules=cythonize([Extension("tenforty.otslib", ["tenforty/otslib/ots.pyx"])]),
-    packages=[*find_packages(exclude=("tests",)), "tenforty.otslib"],
+    ext_modules=cythonize([Extension("tenforty.otslib", ["tenforty/otslib/ots.pyx"])])
 )
